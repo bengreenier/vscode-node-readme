7edfe29466ca735eef9ac0f47d0ff470572355a9
bengreenier <ben@bengreenier.com>
2016-07-20 23:03:26 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,16 @@
+# vscode-node-readme
+
+view installed node_modules readmes
+
+## Features
+
+Quickly open `node_modules` readme files.
+
++ Right click a `require('moduleName')` call in a `js` file
++ Select `View Readme`
++ ...
++ Profit
+
+## License
+
+MIT
---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context) {
+
+    let disposable = vscode.commands.registerCommand('nodeReadme.showReadme', () => {
+        let e = vscode.window.activeTextEditor;
+        let d = e.document;
+
+        let pos = e.selection.start;
+        let line = d.lineAt(pos.line);
+
+        // figure out what module we want
+        let moduleName;
+        let re = /require\(['"]([^'"]+)['"](?:, ['"]([^'"]+)['"])?\);?/g;
+        let str = line.text;
+        let matched;
+        while ((matched = re.exec(str)) != null) {
+            if (matched.index <= pos.character && pos.character <= re.lastIndex) {
+                moduleName = matched[1];
+                break;
+            }
+        }
+
+        let currentUri = d.uri;
+        let readmePath = path.join(currentUri.fsPath, "../", "node_modules", moduleName, "README.md");
+
+        let readmeUri = vscode.Uri.file(readmePath);
+
+        console.log(readmePath);
+
+        fs.exists(readmePath, exists => {
+            if (exists) {
+                return vscode.commands.executeCommand("markdown.showPreviewToSide", readmeUri);          
+            } else {
+                return vscode.window.showErrorMessage(`Module ${moduleName} is not installed locally.`);
+            }
+        });
+    });
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
